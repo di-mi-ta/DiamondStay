a1046135ac8ec076591d8c2ab5a63683dcf10dda
crimsonthinker <kevinnpdk@gmail.com>
2019-05-07 22:44:16 +0700

    Add components

--- code/client/src/components/HomeComponent.js
+++ code/client/src/components/HomeComponent.js
@@ -1,48 +1,38 @@
-import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
-import {Loading} from './LoadingComponent';
-import {baseUrl} from '../shared/baseUrl';
-import {FadeTransform} from 'react-animation-components';
+import React, { Component } from 'react';
+import "../css/main_styles.css";
 
-function RenderCard({item, isLoading, errMess}) {
-    if (isLoading) {
+class Home extends Component{
+    render(){
         return(
-            <Loading />
+            <div class="home">
+                <div class="background_image" ><img src="images/index_1.jpg"/></div>
+                <div class="home_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="home_content text-center">
+                                    <div class="home_title">DiamondStay</div>
+                                    <div class="booking_form_container">
+                                        <form action="#" class="booking_form">
+                                            <div class="d-flex flex-xl-row flex-column align-items-start justify-content-start">
+                                                <div class="booking_input_container d-flex flex-lg-row flex-column align-items-start justify-content-start">
+                                                    <div><input type="text" class="datepicker booking_input booking_input_a booking_in" placeholder="Check in" required="required"/></div>
+                                                    <div><input type="text" class="datepicker booking_input booking_input_a booking_out" placeholder="Check out" required="required"/></div>
+                                                    <div><input type="number" class="booking_input booking_input_b" placeholder="Children" required="required"/></div>
+                                                    <div><input type="number" class="booking_input booking_input_b" placeholder="Room" required="required"/></div>
+                                                </div>
+                                                <div><button class="booking_button trans_200">Book Now</button></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
         );
     }
-    else if (errMess) {
-        return(
-            <h4>{errMess}</h4>
-        );
-    }
-    else
-        return(
-            <FadeTransform in 
-                transformProps={{
-                    exitTransform: 'scale(0.5) translateY(-50%)'
-                }}>
-                {/* TO DO */}
-            </FadeTransform>
-        );
-}
-
-function Home(props) {
-    return(
-        <div className="container">
-            <div className="row align-items-start">
-                <div className="col-12 col-md m-1">
-                    <RenderCard item={props.homepost} 
-                        isLoading={props.homepostsLoading}
-                        errMess={props.homepostsErrMess} />
-                </div>
-                <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} 
-                        isLoading={props.promosLoading}
-                        errMess={props.promosErrMess} />
-                </div>
-            </div>
-        </div>
-    );
 }
 
 export default Home;
