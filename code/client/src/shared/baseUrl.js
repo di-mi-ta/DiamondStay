a1226ee6feacf2b075dbcdc79ea6776ce42c2a2d
Dinh Minh Tan <beta.dimitaa@gmail.com>
2019-05-29 22:16:42 +0700

    tiny  update

--- code/client/src/shared/baseUrl.js
+++ code/client/src/shared/baseUrl.js
@@ -1,5 +1,5 @@
 // deploy
-// export const baseUrl = 'https://bku-diamond-stay-api.herokuapp.com/';
+export const baseUrl = 'https://bku-diamond-stay-api.herokuapp.com/';
 
 // for test
-export const baseUrl = 'http://localhost:4444/';
+// export const baseUrl = 'http://localhost:4444/';
